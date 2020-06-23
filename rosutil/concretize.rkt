c288386658e651ace6f0e83891b28c3965be8d0f
Anish Athalye <me@anishathalye.com>
2020-06-23 08:43:43 -0400

    Add function to concretize all fields in a struct

--- rosutil/concretize.rkt
+++ rosutil/concretize.rkt
@@ -4,7 +4,7 @@
          (prefix-in @ rosette/safe)
          (for-syntax racket/base syntax/parse racket/syntax))
 
-(provide concretize concretize-fields)
+(provide concretize concretize-fields concretize-all-fields)
 
 ; Note: these functions are not general-purpose -- it is in general,
 ; NOT safe to use them in arbitrary Rosette programs. At best,
@@ -16,22 +16,23 @@
 ; (it's all pure functional code), so I think this is okay.
 
 (define (concretize term [error-on-failure #f])
-  (let/ec return
-    (define vars (@symbolics term))
-    (when (empty? vars) (return term))
-    (define model (@solve #t))
-    (when (not (@sat? model))
-      (error "can't find single concrete state"))
-    (define term-concrete (@evaluate term (@complete-solution model (@symbolics term))))
-    (define must-be-same
-      (@unsat?
-       (@verify
-        (@assert
-         (@equal? term term-concrete)))))
-    (cond
-      [must-be-same term-concrete]
-      [(not error-on-failure) term]
-      [else (error 'concretize "failed to concretize term")])))
+  (define vars (@symbolics term))
+  (cond
+    [(empty? vars) term]
+    [else
+     (define model (@solve #t))
+     (when (not (@sat? model))
+       (error "can't find single concrete state"))
+     (define term-concrete (@evaluate term (@complete-solution model vars)))
+     (define must-be-same
+       (@unsat?
+        (@verify
+         (@assert
+          (@equal? term term-concrete)))))
+     (cond
+       [must-be-same term-concrete]
+       [(not error-on-failure) term]
+       [else (error 'concretize "failed to concretize term")])]))
 
 (define-syntax (concretize-fields stx)
   (syntax-parse stx
@@ -44,3 +45,10 @@
           struct-id
           struct-v
           [field-name (concretize (getter-name struct-v))] ...))]))
+
+(define (concretize-all-fields state-constructor state)
+  (apply
+   state-constructor
+   (let ([vec (struct->vector state)])
+     (for/list ([i (in-range 1 (vector-length vec))])
+       (concretize (vector-ref vec i))))))
--- test/rosutil/concretize.rkt
+++ test/rosutil/concretize.rkt
@@ -4,31 +4,49 @@
          rackunit
          (only-in racket/base exn:fail?))
 
-(test-case "concretize concrete"
+(test-case "concretize: concrete"
   (check-equal? (concretize (bv 1337 32)) (bv 1337 32)))
 
-(test-case "concretize basic"
+(test-case "concretize: basic"
   (define-symbolic* x (bitvector 8))
   (define term
     (bveq (bv -1 32) (concat (bv 0 24) x)))
   (check-false (empty? (symbolics term)))
-  (check-not-equal? term #f)
-  (check-equal? (concretize term) #f))
+  (check-not-eq? term #f)
+  (check-eq? (concretize term) #f))
 
-(test-case "concretize larger"
+(test-case "concretize: larger"
   (define-symbolic* x (bitvector 8))
   (define term
     (bveq
      (let ([y (concat (extract 4 4 x) (bv 0 1) (extract 1 0 (concat (bv 0 2) (extract 3 3 x))))])
        (extract 3 2 (bvor y (bvshl y (bv 2 4)))))
      (extract 4 3 x)))
-  (check-not-equal? term #t)
-  (check-equal? (concretize term) #t))
+  (check-not-eq? term #t)
+  (check-eq? (concretize term) #t))
 
-(test-case "concretize failure"
+(test-case "concretize: failure"
   (define-symbolic* x (bitvector 8))
   (define term
     (concat (bv 0 6) (extract 1 0 x)))
   (check-false (empty? (symbolics term)))
   (check-equal? term (concretize term))
   (check-exn exn:fail? (lambda () (concretize term #t))))
+
+(test-case "concretize-fields"
+  (define-symbolic* x (bitvector 8))
+  (struct foo (bar baz) #:transparent)
+  (define f (foo (bveq (bv -1 32) (concat (bv 0 24) x))
+                 (not (bveq (bv -1 32) (concat (bv 0 24) x)))))
+  (define f* (concretize-fields foo f (bar)))
+  (check-eq? (foo-bar f*) #f)
+  (check-not-eq? (foo-baz f*) #t))
+
+(test-case "concretize-all-fields"
+  (define-symbolic* x (bitvector 8))
+  (struct foo (bar baz) #:transparent)
+  (define f (foo (bveq (bv -1 32) (concat (bv 0 24) x))
+                 (not (bveq (bv -1 32) (concat (bv 0 24) x)))))
+  (define f* (concretize-all-fields foo f))
+  (check-eq? (foo-bar f*) #f)
+  (check-eq? (foo-baz f*) #t))
